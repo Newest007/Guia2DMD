1e46cf14b98ae3aed44b830e294bac2ddc87938f
Arian Montes <89265906+Newest007@users.noreply.github.com>
2022-08-27 08:03:11 -0600

    Add files via upload

---
+++ Proyecto_ETL_MM200149/Query Ejercicio2 y Ejercicio.sql
@@ -0,0 +1,19 @@
+Use master
+go
+
+CREATE DATABASE Control_Ventas
+
+USE Control_Ventas
+
+SELECT * FROM [dbo].[query] ORDER BY PedidoID ASC
+
+DROP DATABASE Control_Ventas
+GO
+--------------------------------------------------------
+CREATE DATABASE EjercicioGuia2
+
+USE EjercicioGuia2
+
+SELECT * FROM [dbo].[Query] ORDER BY PedidoID ASC
+
+DROP DATABASE EjercicioGuia2
